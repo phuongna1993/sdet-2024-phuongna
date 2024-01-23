36da0d43e3898c4de77d7a1776fc365eeb1e6ab1
phuongna <anhphuongnguyen03@gmail.com>
2024-01-23 23:59:06 +0700

    commit 2
    day 02

--- src/main/java/day02_operators/bai_01.java
+++ src/main/java/day02_operators/bai_01.java
@@ -1,2 +1,14 @@
-package day02_operators;public class bai_01 {
+package day02_operators;
+
+public class bai_01 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 9;
+        float c = (float)a/b ;
+        System.out.println("Tong cua a va b la: " + (a+b));
+        System.out.println("Hieu cua a va b la: " + (a-b));
+        System.out.println("Tich cua a va b la: " + (a*b));
+        System.out.println("Tich cua a va b la: " + c);
+        System.out.println("Phan du khi b chia a: " + (b%a));
+    }
 }
