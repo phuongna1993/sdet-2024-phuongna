a2f5a719a3d65f66b1df2d4342e44cd60d842e41
phuongna <anhphuongnguyen03@gmail.com>
2024-01-24 09:28:43 +0700

    push code

--- src/main/java/day02_operators/bai_02.java
+++ src/main/java/day02_operators/bai_02.java
@@ -1,2 +1,38 @@
-package day02_operators;public class bai_02 {
+package day02_operators;
+
+public class bai_02 {
+    public static void main(String[] args) {
+        int i1 = 2;
+        int i2 = 5;
+        int i3 = -3;
+        float d1 = 2.0f;
+        float d2 = 5.0f;
+        float d3 = -0.5f;
+
+        System.out.println("ket qua i1 + (i2*i3) la: " + (i1 + (i2*i3)));
+        System.out.println("ket qua i1*(i2+i3) la: " + (i1*(i2+i3)));
+        System.out.println("ket qua i1/(i2+i3) la: " + (i1/(i2+i3)));
+
+        float x = (float)i1/i2;
+        float y = x + (float) i3;
+        System.out.println("ket qua i1/i2+i3 la: " + (x+y));
+
+        int x1 = 3, x2 = 4, x3 = 5;
+        float y1 = x1 + x2 + (float)x3/x1;
+        float y2 = (float) (x1+x2+x3)/3;
+        System.out.println("ket qua cua 3+4+5/3 la: " + y1);
+        System.out.println("ket qua cua (3+4+5)/3 la: " + y2);
+
+        System.out.println("ket qua cua d1+(d2*d3) la: " + (d1+(d2*d3)));
+        System.out.println("ket qua cua d1+d2*d3 la: " + (d1+d2*d3));
+
+        float z1 = d1/d2;
+        float z2 = z1 - d3;
+        System.out.println("ket qua cua d1/d2-d3 la: " + z2);
+        System.out.println("ket qua cua d1/(d2-d3) la: " + (d1/(d2-d3)));
+        System.out.println("ket qua cua d1+d2+d3/3) la: " + (d1+d2+(d3/x1)));
+        System.out.println("ket qua cua (d1+d2+d3)/3 la: " + ((d1+d2+d3)/x1));
+        System.out.println("ket qua cua d1+d2+(d3/3) la: " + ((d1+d2)+(d3/x1)));
+        System.out.println("ket qua cua 3*(d1+d2)*(d1-d3) la: " + (x1*(d1+d2)*(d1-d3)));
+    }
 }
--- src/main/java/day02_operators/bai_03.java
+++ src/main/java/day02_operators/bai_03.java
@@ -1,2 +1,35 @@
-package day02_operators;public class bai_03 {
+package day02_operators;
+
+import org.example.Main;
+
+public class bai_03 {
+    public static void main(String[] args) {
+        /**
+         * Quý khách có tiền vốn là 2 tỷ đồng
+         * gửi tiết kiệm ngân hàng với lãi suất 7.9%/năm.
+         * Sau 10 năm, tổng số tiền cả gốc lẫn lãi nhận được là bao nhiêu?
+         * Gửi lãi đơn
+         * Gửi lãi kép
+         */
+        // Khai bao hang so = so tien ban dau
+        float laiSuat = 0.079f;
+        final int NUMBER_OF_YEAR = 10;
+        final int TIEN_GOC = 2;
+
+        // CT tinh lai don A = P*r*n
+        // CT tinh lai kep A = P(1+r)^n
+        // P: Tien goc
+        // r: % lai
+        // n: Chu ky
+        //Math.pow((1+r),n)
+
+        float tienLaidon = TIEN_GOC*laiSuat*NUMBER_OF_YEAR;
+        float tongTien1 = TIEN_GOC + tienLaidon;
+        double p = Math.pow((1+laiSuat),NUMBER_OF_YEAR)*TIEN_GOC;
+
+        System.out.println("Tong tien thu ve sau 10 nam neu tinh lai don: " + tongTien1 + "ty");
+        System.out.println("Tong tien thu ve sau 10 nam neu tinh lai kep: " + (double) Math.round(p*100)/100 + "ty");
+
+
+    }
 }
